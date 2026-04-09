59c5d2c86c9a42b681c9617e6d52bbf94f113809
Gleidiomar Freitas da Silva <gleidiomar.silva@loglabdigital.com.br>
2026-04-09 09:57:12 -0300

    final fix reset hardcore

--- src/main/java/br/com/case_ebanx/controller/AccountController.java
+++ src/main/java/br/com/case_ebanx/controller/AccountController.java
@@ -5,12 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpStatus;
 
 import br.com.case_ebanx.dto.EventRequest;
 import br.com.case_ebanx.service.AccountService;
@@ -25,9 +22,12 @@ public class AccountController {
     }
     
     @PostMapping("/reset")
-    public void reset(HttpServletResponse response) {
+    public void reset(HttpServletResponse response) throws Exception {
         service.reset();
-        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.reset(); // limpa tudo
+        response.setStatus(200);
+        response.getOutputStream().flush(); // garante vazio total
     }
 
     @GetMapping("/balance")
